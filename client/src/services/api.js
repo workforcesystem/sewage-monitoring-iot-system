0001835cbf5a0a8086718cb810dd3572fc8711db
Workforce Systems <165371405+workforcesystem@users.noreply.github.com>
2026-09-22 20:33:42 +0530

    Change API base URL to relative path

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5050/api",
+  // baseURL: "http://localhost:5050/api",
+  baseURL: "/api",
   withCredentials: true,
 });
 
