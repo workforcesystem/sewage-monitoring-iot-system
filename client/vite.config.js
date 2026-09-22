961cc3dc0724847b896b11ff1daec1ed854fd2b7
Workforce Systems <165371405+workforcesystem@users.noreply.github.com>
2026-09-22 20:35:03 +0530

    Add proxy configuration for API requests

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,4 +7,13 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+
+  server: {
+    proxy: {
+        "/api": {
+            target: "http://localhost:5050",
+            changeOrigin: true,
+        },
+    },
+},
 });
